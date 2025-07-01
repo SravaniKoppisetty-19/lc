70070068b122e8a97eb7aad7f39d88d29b4fc773
SravaniKoppisetty-19 <sravanikoppisetty98@gmail.com>
2025-07-01 13:11:28 +0530

    Time: 2 ms (42.31%), Space: 8.4 MB (19.23%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.c
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.c
@@ -1,10 +1,10 @@
 int possibleStringCount(char* word) {
     int l =strlen(word),sum = 1;
-    for(int i=1;i<l;++i)
+    for(int i=1;i<l;i++)
     {
         if(word[i-1]== word[i])
         {
-            ++sum;
+          sum++;
         }
     }
     return sum;
