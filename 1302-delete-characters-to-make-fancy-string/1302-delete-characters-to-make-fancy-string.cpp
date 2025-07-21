8000c1a4b06176806f654ea4bbeaaf32c9eae62b
SravaniKoppisetty-19 <sravanikoppisetty98@gmail.com>
2025-07-21 10:36:30 +0530

    Time: 30 ms (49.5%), Space: 47 MB (5.59%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int len = s.size();
+       int count = 1;
+       string result = "";
+       for(int i =1;i<len;i++)
+       {
+        if(s[i] == s[i-1])
+        {
+            count++;
+        }
+        else count = 1;
+        if(count <= 2)
+        {
+            result+= s[i];
+        }
+       }
+       result = s[0] + result;
+       return result;    
+
+    }
+};
