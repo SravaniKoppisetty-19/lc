7ece4ef11c3215935223bfbef5031133fe277a28
SravaniKoppisetty-19 <sravanikoppisetty98@gmail.com>
2025-07-23 11:36:13 +0530

    Time: 1 ms (99.74%), Space: 42.1 MB (44.61%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int len = s.length();
+        int sum =0;
+        for(int i = 0;i<len-1;i++)
+        {
+             sum += Math.abs(s.charAt(i) - s.charAt(i+1));
+        }
+        return sum;
+    }
+}
