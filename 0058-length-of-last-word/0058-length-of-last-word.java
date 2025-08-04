5f021cfd931c3fb87c8a8893bba39b3a3ccb2504
SravaniKoppisetty-19 <sravanikoppisetty98@gmail.com>
2025-08-04 14:24:29 +0530

    Time: 1 ms (32.05%), Space: 41.8 MB (65.79%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String word[] = s.split(" ");
+        int n = word.length;
+        int length = word[n-1].length();
+        return length;
+    }
+}
